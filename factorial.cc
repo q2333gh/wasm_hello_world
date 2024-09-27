9705e493835d92ab2a663ae438d1fde8952dade1
btwl <1253102853@qq.com>
2024-09-27 17:35:12 +0800

    impreove

--- factorial.cc
+++ factorial.cc
@@ -4,7 +4,7 @@
 
 // Usage ? 
 // emcc -O3 factorial.cc -o factorial.wasm -s EXPORTED_FUNCTIONS='["_factorial"]' --no-entry
-// becareful the name for cc file of c function : _factorial. when using it . still call factorial in js(or other caller) .
+// Something i dont figure out yet.Maybe doc can tell : becareful the name for cc file of c function : _factorial. when using it . still call factorial in js(or other caller) .
 
 //# on linux : wasmtime run factorial.wasm --invoke factorial 10
 //# on browser: goto on_browser_folder, 
