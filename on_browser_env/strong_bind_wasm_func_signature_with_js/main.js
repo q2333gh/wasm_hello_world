bf840b1122c8090f90fe8da10ce3cfc640e3ab50
btwl <1253102853@qq.com>
2024-09-27 21:18:44 +0800

    improve

--- on_browser_env/strong_bind_wasm_func_signature_with_js/main.js
+++ on_browser_env/strong_bind_wasm_func_signature_with_js/main.js
@@ -1,19 +1,11 @@
-
-// read foo.wasm. find custom function. import it into js with func name, input type , output type.
-
-
-const { loadWasmModule, getFunctionSignature } = require('wasm3');
+const fs = require('fs');
+const path = require('path');
 
 async function loadAndInspectWasm() {
-  const wasmPath = new URL("factorial.wasm", window.location.href).href;
+  const wasmPath = path.resolve(__dirname, 'factorial.wasm');
   console.log("Absolute path for fetch wasm:", wasmPath);
 
-  const response = await fetch(wasmPath);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch WebAssembly file: ${response.statusText}`);
-  }
-
-  const buffer = await response.arrayBuffer();
+  const buffer = fs.readFileSync(wasmPath);
   const module = await WebAssembly.compile(buffer);
   const instance = await WebAssembly.instantiate(module);
 
@@ -23,20 +15,11 @@ async function loadAndInspectWasm() {
   if (typeof instance.exports.factorial !== "function") {
     throw new Error("factorial function is not exported from the WebAssembly module.");
   }
+  // .wat info of func factorial :  (func (;1;) (type 2) (param i32) (result i64)
+  // TODO some js-api must able to read this function input type and output type.
+  // like java reflect.
 
-  // Use wasm3 to get the function signature
-  const signature = getFunctionSignature(instance.exports.factorial);
-  console.log("Function name: factorial");
-  console.log("Input type:", signature.inputTypes);
-  console.log("Output type:", signature.outputTypes);
 
-  exportedFunctions.forEach(funcName => {
-  const func = instance.exports[funcName];
-  if (typeof func === "function") {
-    console.log(`Function name: ${funcName}`);
-  }
-  });
-  
 }
 
 loadAndInspectWasm().catch(console.error);
