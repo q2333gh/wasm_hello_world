8754c3b61b988d2680323a8843f66d111dfc71da
btwl <1253102853@qq.com>
2024-09-27 16:32:15 +0800

    Initial commit

---
+++ factorial.cc
@@ -0,0 +1,22 @@
+#include <stdint.h>
+// What this?
+// simulate cpu intensive ops running by wasm.
+
+// Usage ? 
+// emcc -O3 factorial.cc -o factorial.wasm -s EXPORTED_FUNCTIONS='["_factorial"]' --no-entry
+// becareful the name for cc file of c function : _factorial. when using it . still call factorial in js(or other caller) .
+
+//# on linux : wasmtime run factorial.wasm --invoke factorial 10
+//# on browser: goto on_browser_folder
+
+
+extern "C" {
+    uint64_t factorial(unsigned int n) {
+        uint64_t result = 1;
+        for (unsigned int i = 2; i <= n; ++i) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
---
+++ hello_wasm/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// emcc hello.c -o hello.html
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}
